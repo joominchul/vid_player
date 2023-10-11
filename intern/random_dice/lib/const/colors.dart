707ab05f58a4f0cbd191745f100ecd6690ad50a8
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2023-10-11 17:58:56 +0900

    Delete intern/random_dice directory

--- intern/random_dice/lib/const/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-const backgroundColor = Color(0xff0e0e0e);
-const primaryColor = Colors.white;
-final secondaryColor = Colors.grey[600];
